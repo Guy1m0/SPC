5351abf5699cf6b06524b139ec14f7639403b251
Jingchi Zhang <Kingdomyi@Jingchis-MacBook-Pro.local>
2016-12-06 02:28:58 -0500

    SPC finished, should commit yesterday...

--- DicSetup.py
+++ DicSetup.py
@@ -7,79 +7,141 @@ from dateutil import parser
 from datetime import datetime
 from tweepy.streaming import StreamListener
 
-import json, sys, re, collections, math
+import json, sys, re, collections, math, os
 
 access_token = "3028790692-6AeMsbCuaGZPDuV4MLWH3ymavgiBWFuGvZoVdbN"
 access_token_secret = "Z0kBbO7OC7yC9Z9GJg3DQGYDJvOG5HWAj7XY70ZHPnNIT"
 consumer_key = "nEHpCxgQjOLpfHXAAmlWzxwYF"
 consumer_secret = "Sfa8e7tZPC4gDS2QYVQ3ykqMYHsC7gNhbaWtBTeXwMhbAbIAcO"
 
-msg_filters_format = ["https://", "&"]
-msg_filters_words = []
-search_number = 5000
+msg_filters_format = ["https://", "&", "RT", "@"]
+msg_filters_out = ["#Ad"]
+msg_filters_words = ["https", "rt"]
+
+cnt_tf = collections.Counter()
+cnt_idf = collections.Counter()
+cnt_tf_idf = collections.Counter()
+
+level = []
+
+search_number = 0
 
 #use for streaming
 class StdOutListener(StreamListener):
-	count = 0
+	num = 0
 	hashtag = ''
+	buffer = ""
 
-	def __init__(self, hashtag, api):
+	def __init__(self, hashtag, num, api):
 		self.api = api
 		self.hashtag = hashtag
-		self.f = open(hashtag + '.txt', "w+")
-		print self.hashtag
+		self.num = num
+		#self.f = open(hashtag + '.txt', "w+")
+		#print self.hashtag
 
 	def on_status(self, status):
+		f = open('steaming_result.txt', "w+")
 		#print tag
 		#print "running?"
-		if (self.count < size):
-			#f.write("original:" + status._json['text'].encode("utf-8") + "\n")
-			text = takeout(status._json['text'])
-			self.f.write(text + "\n")	
-			#print status._json['text'].encode("utf-8")
-			self.count = self.count + 1	
-			print "count:", self.count
-		else:
-			self.f.close()
-			sys.exit("Collect Finished")
+		print "tweet:", status._json['text'].encode("utf-8")
+
+		msg = status._json['text'].encode("utf-8")
+		self.buffer += msg + "\n"
+		self.buffer += "1 (Highest Risk) -----> 5 (Lowest Risk) \n"
+		self.buffer += "This tweet risk is " + calculate_level(msg) + "\n"
+		
+		self.num -= 1
+
+		if self.num == 0:
+			f.write(self.buffer)
+			f.close()
+			sys.exit("streaming finished")
+		f.close()
+		#check_msg(msg, '#TrumpFirsts')
 
 	def on_error(self, status):
 		print status
 
-def takeout(msg, filters = msg_filters_format):
-	msg = msg.encode("utf-8")
-		#cut from the ':'
-	index = msg.find(":")
-	msg = msg[index + 1:]
+def takeout(msg, filter_format = msg_filters_format, filter_words = msg_filters_words):
 	#print "original:", 	msg
-
-	for char in filters:
+	msg = msg.lower()
+	for char in filter_format:
 		#print char
 		buffer = ""
 		for word in msg.split():
+			#print "word:", word
 			if char not in word:
 				buffer = buffer + word + " "
 
 		msg = buffer
+	msg = msg[:-1]
 
-	msg = re.sub('[^a-zA-Z0-9\- ]+', '', msg)
-	#print msg
+	for words in filter_words:
+		words = words.lower()
+		#print char
+		buffer = ""
+		for word in msg.split():
+			#print "word:", word
+			if words != word:
+				buffer = buffer + word + " "
+
+		msg = buffer
+
+	msg = re.sub('[^a-zA-Z0-9\-\# ]+', '', msg)
 
 	return msg[:-1]
 
-def check_msg(msg, hashtag):
-	#tag = hashtag
-	#__set_up_dict(hashtag)
+def calculate_level(msg):
+	ssum = 0
+	f = open('streaming_log.txt', "a+")
+	#f.close()
+	#f = open('streaming_log.txt', "a")
+	print >> f, msg, "\n"
 	msg = takeout(msg)
-	__calculate_relevance(msg, hashtag)
 
+	filter_words = re.findall(r'\w+', open('frequency.txt').read().lower())
+
+	print >> f, "---------------------------Analysis-------------------------------\n"
+	for words in filter_words:
+		buffer = ""
+		for word in msg.lower().split():
+			
+			if words != word:
+				buffer = buffer + word + " "
+
+		msg = buffer
+
+	msg = msg[:-1]
+
+	for word in msg.lower().split():
+		
+		idf= float(search_number + 1)/(cnt_idf[word] + 1)
+		print >> f, "word:", word, "inverse document frequcney:", idf, "\n"
+		ssum += idf
+
+	print >> f, "total value:", ssum, "effective words:", len(msg.split()), "\n"
+	ssum = ssum/len(msg.split())
+	print >> f, "risk value:", ssum, "\n"
+	print >> f, "risk level break point:", level, "\n"
+
+	for i in range(4):
+		if ssum > level[i]:
+			print >> f, "risk level:", i+1
+			print >> f, "--------------------------------------------------------------"
+			f.close()
+			return str(i + 1)
+
+	print >> f, "no risk"
+	print >> f, "--------------------------------------------------------------"
+	f.close()
+	return str(5)
 
 def __set_up_dict(hashtag):
 	f = open(hashtag + '.txt', "w+")
+	g = open(hashtag + '_bak.txt', "w+")
 
 	auth = OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
-	
 	api = tweepy.API(auth)
 
 	search_text = hashtag
@@ -93,7 +155,20 @@ def __set_up_dict(hashtag):
 			#print len(new_tweets)
 
 			for msg in new_tweets:
+				msg = msg.text.encode("utf-8")
+
+				#filter out any message contain rubbish
+				for filter_item in msg_filters_out:
+					if filter_item in msg.split():
+						msg = ""
+
+				if msg == "":
+					break
+
+				g.write("original:" + msg + "\n")
 				tweet = takeout(msg)
+				g.write("modified:" + tweet + "\n")
+				f.write(tweet + "\n")
 				#print msg
 				search_result.append(tweet)
 			if not new_tweets:
@@ -102,37 +177,31 @@ def __set_up_dict(hashtag):
 		except tweepy.TweepError as e:
 			break
 
-	print "start to remove duplicate"
-	result = list(set(search_result))
-	print len(result)
-
-	for msg in result:
-		f.write(msg + "\n")
-
 	f.close()
-	sys.exit("collect finished")
+	g.close()
 
-	# use for the streaming
-	'''
-	listener = StdOutListener(hashtag, api)
-	stream = Stream(auth, listener)
+def __update_relevance(hashtag):
+	cnt = collections.Counter()	
+	total = 0
 
-	#print hashtag
-	stream.filter(track = ['#TrumpFirsts'])#[hashtag])
-	'''
+	with open(hashtag + '.txt') as f:
 
-def __calculate_relevance(msg, hashtag):
-	#f = open(hashtag + '.txt', "r")
-	words = re.findall(r'\w+', open(hashtag + '.txt').read().lower())
-	cnt = collections.Counter(words)	
+		for word in f.read().split():
+			#print "reading?"
+			word = word.lower()
+			#print word
+			cnt[word] += 1
+			total += 1
+	f.close()
 
-	print cnt.most_common(30)
+	#use frequency dictionary to filter out common words
+	filter_words = re.findall(r'\w+', open('frequency.txt').read().lower())
+	filter_words.append('https')
+	for word in filter_words:
+		del cnt[word]
 
-	cnt_tf = collections.Counter()
-	cnt_idf = collections.Counter()
-	cnt_tf_idf = collections.Counter()
-
-	total = len(cnt)
+	#total tweets in the database,
+	num_tweet = cnt[hashtag.lower()]
 
 	for word in list(cnt):
 		line_count = 0
@@ -145,32 +214,123 @@ def __calculate_relevance(msg, hashtag):
 						cnt_idf[word] += 1
 						break
 
-			if (cnt[word] == 0):
+			#cnt_idf[word] = math.log10(float(line_count)/cnt_idf[word])
 
-				cnt_tf_idf[word] = 0
-				#break
-			else:
-				print "word:",word, cnt_idf[word], cnt[word]
-				cnt_idf[word] = math.log10(float(line_count)/cnt_idf[word])
-			#print word, cnt_idf[word]
-			#print "tf:",cnt_tf[word],total
-				cnt_tf[word] = float(cnt_tf[word])/total
-			#print cnt_tf[word]
-				cnt_tf_idf[word] = cnt_tf[word] * cnt_idf[word]
-	sum = 0
-	for word in msg.lower().split():
-		print "%s tf_idf is: %f", word, cnt_tf_idf[word] 
-		sum += cnt_tf_idf[word]
+			cnt_tf[word] = float(cnt[word])/total
 
-	print sum
+	#print "test match", num_tweet, line_count
+
+	f.close()
+
+	#calculate the expectation level for the database
+	tweet_level = []
+	index = 0
+
+	with open (hashtag + '.txt') as f:
+
+		for line in f:
+
+			ssum = 0
+			for words in filter_words:
+				buffer = ""
+				for word in line.lower().split():
+		
+					if words != word:
+						buffer = buffer + word + " "
+
+				line = buffer
+
+			line = line[:-1]
+			if len(line) != 0:
+				for word in line.lower().split():
+		
+					idf= float(search_number + 1)/(cnt_idf[word] + 1)
+			#print "%s tf_idf is: %f", word, idf
+				ssum += idf
+				#print "line:", line, len(line)
+				#print ssum, len(msg.split())
+				ssum = ssum/len(line.split())
+
+				tweet_level.append(ssum)
+				index +=1
+				ssum = 0
+
+	f.close()
+	#print "h", tweet_level
+	tweet_level = sorted(tweet_level, key = float, reverse = True)
+	
+	level.append(tweet_level[int(search_number*0.001)])
+	level.append(tweet_level[int(search_number*0.005)])
+	level.append(tweet_level[int(search_number*0.02)])
+	level.append(tweet_level[int(search_number*0.05)])
+	#level.append(tweet_level[int(search_number*0.2)])
+
+	print level
+
+
+	#print cnt_tf_idf['of']
+USAGE = """
+usage: DicSetup <database size> <Hashtag> <mode> <parm>
+       database size: the num of tweets collected from twitter
+  	   mode: either 'streaming' or 'msg'
+  	   parm: int number of the incoming tweets for mode 'streaming'
+  	   		 or string for the mode 'msg'
+       Default: 2000 '#Trump' streaming 5
+       Running sample: python DicSetup.py 5000 NBA streaming 10
+       			   or: python DicSetup.py 5000 NBA msg " rt XXX..."
+       """
+
 
 if __name__ == '__main__':
-	msg = "RT @crazylary51: #TrumpFirsts all the fuss Trump created about 1,000 jobs when @POTUS has created 16 million without bragging. https://t.c"
-	check_msg(msg, '#TrumpFirsts')
 
-	#msg = msg.encode("utf-8")
-	#takeout(msg, ["https://","#", "@"])
+ 	args = sys.argv[1:]
+
+	if args == []:
+		args.append(2000)
+		args.append('#Trump')
+		args.append("streaming")
+		args.append(5)
+	elif len(args) != 4:
+		print "here?"
+		sys.exit(USAGE)
+
+
+	search_number = int(args[0])
+
+	hashtag = args[1]
+	hashtag = "#" + re.sub('[^a-zA-Z0-9]+', '', hashtag)
+
+	msg_filters_words.append(hashtag)
+
+	if not os.path.isfile(hashtag + '.txt'):
+		__set_up_dict(hashtag)
+		print "dict finished"
+
+	__update_relevance(hashtag)
 	
+	if args[2] == "msg":
+
+		msg = args[3]
+		calculate_level(msg)
+		#calculate_level(msg1)
+
+	#check_msg(msg, '#TrumpFirsts')
+	#check_msg(msg2, '#TrumpFirsts')
+	elif args[2] == "streaming":
+		num = int(args[3])
+		
+		auth = OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_token, access_token_secret)
+	
+		api = tweepy.API(auth)
+		listener = StdOutListener(hashtag, num, api)
+		stream = Stream(auth, listener)
+
+	#print hashtag
+		stream.filter(track = [hashtag])#[hashtag])
+	else:
+		sys.exit(USAGE)
+
 	#print "running"
 
 	
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# SPC
+Running
+----------------------------------------------------------------------------
+usage: DicSetup <database size> <Hashtag> <mode> <parm>
+       database size: the num of tweets collected from twitter
+  	   mode: either 'streaming' or 'msg'
+  	   parm: int number of the incoming tweets for mode 'streaming'
+  	   		 or string for the mode 'msg'
+       Default: 2000 '#Trump' streaming 5
+       Running sample: python DicSetup.py 5000 NBA streaming 10
+              	   or: python DicSetup.py 5000 NBA msg " rt XXX..."
+
+Files
+----------------------------------------------------------------------------
+System will generate the dictionary file with the name of hashtag and another file for debug only
+
+After the system collected tweets from internet, the tweet information will store in the file with name "streaming_result.txt" with the risk level. Meanwhile, the log file will be generated for checking the computation of the risk level.
+
+frequency.txt is the file the most common words in Project Gutenberg: These lists are the most frequent words, when performing a simple, straight (obvious) frequency count of all the books found on Project Gutenberg. I took the first 300 words from the Project Gutenberg
+
+Notice
+----------------------------------------------------------------------------
+If the dictionary file is already on the disk, but you want to change the search_number, maybe from 5000 to 2000, please delete the dictionay file and then rerun the code
+
+For the mode 'msg', the result will not be stored in the file, 'streaming_result.txt' but only store in the file, 'streaming_log.txt'
